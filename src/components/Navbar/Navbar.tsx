49cb73f019de2f5c98fa6d6f4f43e0771d604ac0
René Morgado <renemorgado33@gmail.com>
2024-08-02 14:25:54 -0600

    feat(js): refactor de estilos para navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,35 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.scss'
 import { NavLink } from 'react-router-dom'
-import Contact from './img/contact.svg'
-import Cards from './img/cards.svg'
-import Card from './img/card.svg'
-import AddCard from './img/cards-add.svg'
-
-
+import Profile from './img/profile.png'
+import Menu from './img/menu.svg'
+import Close from './img/close.svg'
 
 const Navbar = () => {
+
+    const [showNavigation, setShowNavigation] = useState<boolean>(false)
+
+    const toggleNavigation = (): void => {
+        setShowNavigation(!showNavigation)
+    }
+
+
     return (
-        <div className="Navbar">
-            <h3 className='ms-4 mb-0 d-sm-block d-none'>Tarjetas App</h3>
-            <NavLink to="/" className={({ isActive }) => (isActive ? 'Navbar__link--active' : 'Navbar__link')}>
-                <img src={Card} alt='contact' />
-                <span>Inicio</span>
-            </NavLink>
-            <NavLink to="/cards" className={({ isActive }) => (isActive ? 'Navbar__link--active' : 'Navbar__link')}>
-                <img src={Cards} alt='contact' />
-                <span>Mis Tarjetas</span>
-            </NavLink>
-            <NavLink to="/add-card" className={({ isActive }) => (isActive ? 'Navbar__link--active' : 'Navbar__link')}>
-                <img src={AddCard} alt='contact' />
-                <span>Agregar Tarjeta</span>
-            </NavLink>
-            <NavLink to="/contact" className={({ isActive }) => (isActive ? 'Navbar__link--active' : 'Navbar__link')}>
-                <img src={Contact} alt='contact' />
-                <span>Contáctanos</span>
-            </NavLink>
+        <div className="Navbar p-sm-0 p-4 d-flex justify-content-between justify-content-sm-around align-items-center">
+            <button onClick={toggleNavigation} className="Navbar__button d-flex d-sm-none" type="button">
+                <img src={Menu} alt="menu" />
+            </button>
+            <div className={showNavigation ? 'Navbar__navigation' : 'Navbar__navigation--closed'}>
+                {
+                    showNavigation && <div className='p-3'>
+                        <div className="Navbar__navigation--header d-flex justify-content-between align-items-center pb-3">
+                            <p className='fw-semibold mb-0 fs-4'>CardsApp</p>
+                            <button onClick={toggleNavigation} className="Navbar__button d-flex d-sm-none" type="button">
+                                <img src={Close} alt="menu" />
+                            </button>
+                        </div>
+                        <div className='Navbar__navigation--links d-flex flex-column align-items-center mt-4'>
+                            <NavLink onClick={toggleNavigation} to="/" className={({ isActive }) => (isActive ? 'active my-2' : 'my-2')}>
+                                <span className='fw-medium '>Inicio</span>
+                            </NavLink>
+                            <NavLink onClick={toggleNavigation} to="/cards" className={({ isActive }) => (isActive ? 'active my-2' : 'my-2')}>
+                                <span className='my-4 fw-medium '>Mis Tarjetas</span>
+                            </NavLink>
+                            <NavLink onClick={toggleNavigation} to="/add-card" className={({ isActive }) => (isActive ? 'active my-2' : 'my-2')}>
+                                <span className='me-md-4 me-2 fw-medium '>Agregar Tarjeta</span>
+                            </NavLink>
+                            <NavLink onClick={toggleNavigation} to="/contact" className={({ isActive }) => (isActive ? 'active my-2' : 'my-2')}>
+                                <span className='me-md-4 me-2 fw-medium '>Contáctanos</span>
+                            </NavLink>
+                        </div>
+                    </div>
+                }
+            </div>
+            <div className="Navbar__logo d-none d-sm-flex">
+                <p className='fw-semibold'>CardsApp</p>
+            </div>
+            <div className="Navbar__links d-none d-sm-flex">
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <span className='me-md-4 me-2 fw-medium '>Inicio</span>
+                </NavLink>
+                <NavLink to="/cards" className="">
+                    <span className='me-md-4 me-2 fw-medium '>Mis Tarjetas</span>
+                </NavLink>
+                <NavLink to="/add-card" className="">
+                    <span className='me-md-4 me-2 fw-medium '>Agregar Tarjeta</span>
+                </NavLink>
+                <NavLink to="/contact" className="">
+                    <span className='me-md-4 me-2 fw-medium '>Contáctanos</span>
+                </NavLink>
+            </div>
+            <div className="Navbar__profile">
+                <img src={Profile} alt="profile pic" />
+            </div>
         </div>
     )
 }
 
 export default Navbar
+
+
+/*       <div className="Navbar">
+<h3 className='ms-4 mb-0 d-sm-block d-none'>Tarjetas App</h3>
+
+</div>*/
